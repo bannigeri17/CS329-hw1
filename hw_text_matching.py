4e0961bf22bbed455416eadae13647ba8d60d299
bannigeri17 <bannige@emory.edu>
2020-02-15 13:23:10 -0500

    begin to build dialog manager, modify ontology to handle diverse inputs

--- hw_text_matching.py
+++ hw_text_matching.py
@@ -1,12 +1,31 @@
 from emora_stdm import KnowledgeBase, DialogueFlow
-from enum import Enum
+from enum import Enum, auto
+import json
 
 
 # TODO: Update the State enum as needed
 class State(Enum):
     START = 0
-    PROMPT = 1
-    ERR = 2
+    INIT_PROMPT = auto()
+    QUES1 = auto() #What do you most often play video games on?
+    ANS1 = auto()
+
+    #console brands/device types
+    ATARI = auto()
+    PLAYSTATION = auto()
+    NINTENDO = auto()
+    XBOX = auto()
+    PC = auto()
+    SEGA = auto()
+
+    #edge cases for further specifying types of devices
+    GAMEBOY = auto()
+    DS = auto()
+    GENESIS = auto()
+
+    QUES2 = auto()
+
+    ERR = auto()
 
 
 # TODO: create the ontology as needed
@@ -21,8 +40,14 @@ knowledge = KnowledgeBase()
 knowledge.load_json(ontology)
 df = DialogueFlow(State.START, initial_speaker=DialogueFlow.Speaker.SYSTEM, kb=knowledge)
 
-df.add_system_transition(State.START, State.PROMPT, '"Hi, what do you want to talk about?"')
-df.set_error_successor(State.PROMPT, State.ERR)
+df.add_system_transition(State.START, State.INIT_PROMPT, '"Hi, do you play video games?"')
+df.add_user_transition(State.INIT_PROMPT,State.QUES1,"{yes,yeah}")
+df.add_system_transition(State.QUES1,State.ANS1,'"What do you most often play video games on?"')
+df.add_user_transition(State.ANS1,State.ATARI,'$device={#ONT(atari),atari}')
+df.add_user_transition(State.ANS1,State.PLAYSTATION,'$device=#ONT(playstation)')
+df.add_user_transition(State.ANS1,State.DS,'$device={#ONT(ds),ds}')
+df.add_user_transition(State.ANS1,State.NINTENDO,'$device=#ONT(nintendo)')
+df.set_error_successor(State.INIT_PROMPT, State.ERR)
 # TODO: create your own dialogue manager
 
 
