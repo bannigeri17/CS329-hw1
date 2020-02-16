b27b7dbd663b80110312f364aaa48fb112da64a3
bannigeri17 <bannige@emory.edu>
2020-02-16 14:45:17 -0500

    Add few more states, continue working through conversation flow... with an emora-stdm upgrade, it works now :)

--- hw_text_matching.py
+++ hw_text_matching.py
@@ -6,6 +6,7 @@ import json, csv
 
 vg_dict = {} #a dictionary mapping a game title to various data points about it, i.e. platform, release year and genre
 sys_favs = {} #a dictionary using vgsales.csv to map platforms to an arbitrary favorite game from that platform
+genre = None
 
 with open("vgsales.csv", newline='') as csvfile:
     file_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
@@ -24,6 +25,17 @@ class SYSTEM_FAV(Macro):
             if vars['device']  in sys_favs:
                 return "? My favorite is " + sys_favs[vars['device']]+"!"
             return "? I don't really have a favorite for that device."
+
+# TODO: Maybe update conversation flow/state transitions so we don't have to do anything complicated with genre classification
+class FAV_GAME_GENRE(Macro):
+    def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[Any]):
+        if 'fav_game' in vars:
+            print(vars['fav_game'])
+            if vars['fav_game']  in vg_dict:
+                genre = vg_dict['fav_game']
+                vars['genre'] = genre
+                return "Do you prefer "+genre+"games?"
+            return "Do you have a favorite genre?"
 
 # TODO: Update the State enum as needed
 class State(Enum):
@@ -51,8 +63,17 @@ class State(Enum):
     QUES2 = auto()  # Is there anything you like about using a $brand device?
     ANS2 = auto()
 
-    QUES3 = auto()
+    QUES3 = auto() #What's your favorite game to play on your $device?
     ANS3 = auto()
+
+    QUES3b = auto() #do you have a favorite genre of video games?
+    ANS3b = auto()
+
+    QUES4 = auto() #Do you prefer $genre games?
+    ANS4 = auto()
+
+    QUES5 = auto() #What do you like about $genre games?
+    ANS5 = auto()
 
     ERR = auto()
 
@@ -125,15 +146,16 @@ ontology = {
 
 knowledge = KnowledgeBase()
 knowledge.load_json(ontology)
-df = DialogueFlow(State.START, initial_speaker=DialogueFlow.Speaker.SYSTEM, kb=knowledge, macros={"SYSTEM_FAV":SYSTEM_FAV()})
+df = DialogueFlow(State.START, initial_speaker=DialogueFlow.Speaker.SYSTEM, kb=knowledge, macros={"SYSTEM_FAV":SYSTEM_FAV(),
+                                                                                                  "FAV_GAME_GENRE":FAV_GAME_GENRE()})
 
 df.add_system_transition(State.START, State.INIT_PROMPT, '"Hi, do you play video games?"')
 df.add_user_transition(State.INIT_PROMPT, State.QUES1, "{yes,yeah}")
 
 df.add_system_transition(State.QUES1, State.ANS1, '"What do you most often play video games on?"')
-df.add_user_transition(State.ANS1, State.ATARI, r"[$device=#ONT(atari)]")
-df.add_user_transition(State.ANS1, State.PLAYSTATION, r"[$device={#ONT(playstation),playstation}]")
-df.add_user_transition(State.ANS1, State.DS, r"[$device={#ONT(ds),ds}]")
+df.add_user_transition(State.ANS1, State.ATARI, "[$device=#ONT(atari)]")
+df.add_user_transition(State.ANS1, State.PLAYSTATION, "[$device={#ONT(playstation),playstation}]")
+df.add_user_transition(State.ANS1, State.DS, "[$device={#ONT(ds),ds}]")
 df.add_user_transition(State.ANS1, State.GAMEBOY, "[$device={#ONT(gameboy),gameboy}]")
 df.add_user_transition(State.ANS1, State.NINTENDO, "[$device=#ONT(nintendo)]")
 df.add_user_transition(State.ANS1, State.XBOX, "[$device={#ONT(xbox),xbox}]")
@@ -145,7 +167,7 @@ df.add_system_transition(State.UNNAMED_CONSOLE, State.ANS1, '"What kind of conso
 df.add_system_transition(State.DS, State.DS_ANS, '"What kind of DS do you have?"')
 df.add_user_transition(State.DS_ANS, State.NINTENDO, '"[$device=#ONT(ds)]"')
 df.add_system_transition(State.GAMEBOY, State.GAMEBOY_ANS, '"What kind of Gameboy do you have?"')
-df.add_user_transition(State.GAMEBOY_ANS, State.NINTENDO, '"[$device = #ONT(ds)]"')
+df.add_user_transition(State.GAMEBOY_ANS, State.NINTENDO, '"[$device = #ONT(gameboy)]"')
 
 df.add_system_transition(State.ATARI, State.QUES2, '"Is there anything you like about using Atari?"')
 df.add_system_transition(State.PLAYSTATION, State.QUES2, '"Is there anything you like about using a Playstation console?"')
@@ -156,14 +178,24 @@ df.add_system_transition(State.SEGA, State.QUES2, '"Is there anything you like a
 
 df.add_user_transition(State.QUES2, State.ANS2, "[{no,not really,nah}]")
 df.add_user_transition(State.QUES2, State.QUES3, "[-{no,not really,nah}]")
-df.add_system_transition(State.ANS2, State.QUES3, "thats too bad")
+df.add_system_transition(State.ANS2, State.ANS3, "well do you at least have a favorite game to play on your $device #SYSTEM_FAV")
 
 df.add_system_transition(State.QUES3,State.ANS3, "whats your favorite game to play on your $device #SYSTEM_FAV")
 
-#df.add_user_transition(State.ANS3,State.QUES4,"[$fav_game = ]")
+# TODO: FIGURE OUT HOW TO ASSIGN VARIABLE DIRECTLY TO INPUT TEXT, OR HOW TO RECEIVE THE GAME INPUT
+df.add_user_transition(State.ANS3,State.QUES4,"$fav_game=[-{none,dont have one}]")
+df.add_user_transition(State.ANS3,State.QUES3b,"[{none,dont have one}]")
 
+df.add_system_transition(State.QUES3b, State.ANS3b, '"Do you have a favorite genre of video game?"')
+df.add_user_transition()
+
+df.add_system_transition(State.QUES4,State.ANS4,"#FAV_GAME_GENRE")
+
+df.add_user_transition(State.ANS4, State.QUES5, "")
+
+# TODO: CREATE ERROR SUCCESSORS-ENSURE ROBUSTNESS
 df.set_error_successor(State.INIT_PROMPT, State.ERR)
-# TODO: create your own dialogue manager
+
 
 
 df.run(debugging=False)
