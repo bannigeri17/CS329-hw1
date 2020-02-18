6e119aa387bf1d71b54eb4d1297fb0ee1803ca10
Simon Marty <simon.marty0@gmail.com>
2020-02-17 21:35:55 -0500

    Load JSON from file, add some states

--- test_videogames.py
+++ test_videogames.py
@@ -1,6 +1,7 @@
 # Test class for videogames.py
 
 import videogames as v
+import json
 
 if __name__ == '__main__':
     print(v.get_console_game_sales('X360'))
@@ -8,5 +9,7 @@ if __name__ == '__main__':
     print(v.get_sales_for_game('Super Mario Bros'))
     print(v.get_best_selling_game('Wii'))
     print(v.get_console_game_time_range('Wii'))
-    for i in range(20):
-        print(v.get_random_game_from_genre())
+    # for i in range(20):
+    #     print(v.get_random_game_from_genre())
+    s = json.loads(open('gaming_ontology.json').read())
+    print(s)
